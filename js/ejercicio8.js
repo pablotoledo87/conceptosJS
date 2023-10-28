762b11e336dae868c00fb08f44322e1546269f44
Juan Pablo Toledo <toledo.pablo87@gmail.com>
2023-10-28 20:49:04 -0300

    agregamos ejercicio 8

---
+++ js/ejercicio8.js
@@ -0,0 +1,6 @@
+const divisible = parseInt(prompt("ingrese un valor"));
+if(divisible %2 === 0){
+    alert("El numero " +(divisible)+ " es divisible por 2");
+}else{
+    alert("el numero " +(divisible)+ " NO es divisible por 2")
+}
