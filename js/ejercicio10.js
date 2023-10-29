0a12a2d15df3d4781a22c45f48f04b9a0858efeb
Juan Pablo Toledo <toledo.pablo87@gmail.com>
2023-10-28 21:01:31 -0300

    agregamos ejercicio 10

---
+++ js/ejercicio10.js
@@ -0,0 +1,13 @@
+const div = parseInt(prompt("ingrese un valor"));
+if(div %2 === 0){
+    alert("El numero es divisible por 2");
+}
+if(div %3 === 0){
+    alert("El numero es divisible por 3");
+}
+if(div %5 === 0){
+    alert("El numero es divisible por 5");
+}
+if(div %7 === 0){
+    alert("El numero es divisible por 7");
+}
