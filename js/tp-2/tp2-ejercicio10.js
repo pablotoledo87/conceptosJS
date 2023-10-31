9bb53198871e97552adbd5e389abae174f77cb46
Juan Pablo Toledo <toledo.pablo87@gmail.com>
2023-10-30 21:34:31 -0300

    actualizamos ejercicio10 tp2

--- js/tp-2/tp2-ejercicio10.js
+++ js/tp-2/tp2-ejercicio10.js
@@ -2,13 +2,14 @@
 const filas = parseInt(prompt("ingrese un numero de fila"))
 const columnas = parseInt(prompt("ingrese un numero de columnas"))
 
-const totalCeldas = filas * columnas;
+let totalCeldas = filas * columnas;
 
 document.write(`<table><tbody>`);
 for(let indiceFilas = 0; indiceFilas< filas;indiceFilas++ ){
     document.write(`<tr>`);
     for(let indiceColumnas = 0;indiceColumnas< columnas; indiceColumnas++){
-        document.write(`<td>35</td>`);
+        document.write(`<td>${totalCeldas}</td>`);
+        totalCeldas--;
     }
     
     document.write(`</tr>`);
