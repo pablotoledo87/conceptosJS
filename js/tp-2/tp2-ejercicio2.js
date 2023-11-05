fe3156c4d14f17f205d491535b7a5f1583273831
Juan Pablo Toledo <toledo.pablo87@gmail.com>
2023-11-05 20:35:57 -0300

    agregamos ejercicio 2 del tp2

---
+++ js/tp-2/tp2-ejercicio2.js
@@ -0,0 +1,39 @@
+
+const calificacion = (prompt("Ìngrese su calificacion"))
+
+switch(calificacion){
+    case '0':
+    case '1':
+    case '2':
+        
+        document.write(`Su calificacion es de ${calificacion}, es muy deficiente`)
+        break;
+
+    case '3':
+    case '4':
+            
+    document.write(`Su calificacion es de ${calificacion}, es insuficiente`)
+    break;
+        
+    case '5':
+    case '6':
+                    
+    document.write(`Su calificacion es de ${calificacion}, es suficiente`)
+    break;
+    case '7':
+   
+            
+    document.write(`Su calificacion es de ${calificacion}, bien`)
+    break;
+    case '8':
+    case '9':
+                    
+    document.write(`Su calificacion es de ${calificacion}, es notable`)
+    break;
+    case '10':
+                    
+    document.write(`Su calificacion es de ${calificacion}, es notable`)
+    break;
+    default:
+        document.write("numero erroneo");
+}
